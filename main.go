25a5d7966e08c0bebb032b55dee730454df6d5eb
Daniel Santos <danielsantos@c6bank.com>
2020-04-13 13:37:45 -0300

    fix time func

--- main.go
+++ main.go
@@ -18,22 +18,23 @@ func init() {
 	startTime = time.Now()
 }
 
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+    t := time.Now().Format(time.RFC1123)
+    u := uptime()
+    fmt.Fprintf(w, "Time: %s | uptime: %s | version: %s", t , u, version)
+}
+
 const version string = "1.2.3-4"
 
 // VersionHandler handles incoming requests to /version
 // and just returns a simple version number
 func versionHandler(w http.ResponseWriter, r *http.Request) {
-    t := time.Now().Format(time.RFC1123)
-    u := uptime()
-    //Body := "The current time is:"
-fmt.Fprintf(w, "Time: %s | uptime: %s | version: ", t , u)
-
-
 	io.WriteString(w, version)
 }
 
 func main() {
 	log.Printf("Listening on port 8080...")
 	http.HandleFunc("/version", versionHandler)
+	http.HandleFunc("/status", statusHandler)
 	http.ListenAndServe(":8080", nil)
 }
